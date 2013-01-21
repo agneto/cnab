6088ff40d66c4983675975ec90be67114d3e7a20
Diego Fincatto <diego@fincatto.com>
2013-01-21 10:12:52 -0200

    Testes unitarios com novo framework do junit

--- src/main/java/com/google/code/cnab/App.java
+++ src/main/java/com/google/code/cnab/App.java
@@ -1,8 +1,5 @@
 package com.google.code.cnab;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(final String[] args) {
         System.out.println("Oi mundo!");
--- src/test/java/com/google/code/cnab/AppTest.java
+++ src/test/java/com/google/code/cnab/AppTest.java
@@ -1,33 +1,12 @@
 package com.google.code.cnab;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     * 
-     * @param testName name of the test case
-     */
-    public AppTest(final String testName) {
-        super(testName);
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testApp() {
-        TestCase.assertTrue(true);
+        Assert.assertTrue(true);
     }
 }
