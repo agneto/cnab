03384b270c8865ab3ce62af144a8c93dbe6c1d4c
Diego Fincatto <diego@fincatto.com>
2013-01-21 09:47:00 -0200

    Ajustando ignores.

--- src/main/java/com/google/code/cnab/App.java
+++ src/main/java/com/google/code/cnab/App.java
@@ -5,6 +5,6 @@ package com.google.code.cnab;
  */
 public class App {
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Oi mundo!");
     }
 }
