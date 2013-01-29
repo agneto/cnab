40a80a12fbedba15cd58489fdc8f89250b679ba6
Abilio Neto <sis.neto@gmail.com>
2013-01-29 15:55:25 -0200

    Testes e implementacoes de leitura

--- src/main/java/com/google/code/cnab/App.java
+++ src/main/java/com/google/code/cnab/App.java
@@ -1,7 +1,50 @@
 package com.google.code.cnab;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.code.cnab.arquivoretorno.Header;
+import com.google.code.cnab.arquivoretorno.Registro;
+import com.google.code.cnab.arquivoretorno.Trailer;
+
 public class App {
     public static void main(final String[] args) {
-        System.out.println("Oi mundo!");
+        // System.out.println("Oi mundo!");
+        new App().lerArquivo("/Users/abilio/Downloads/R19011.RET.txt");
+    }
+
+    public void lerArquivo(final String caminhoArquivo) {
+        String linha = null;
+
+        final List<Registro> registros = new ArrayList<Registro>();
+        try (FileReader reader = new FileReader(caminhoArquivo)) {
+            try (BufferedReader leitor = new BufferedReader(reader)) {
+
+                while ((linha = leitor.readLine()) != null) {
+                    // leitura do header
+                    if (linha.startsWith("0")) {
+                        new Header(linha);
+                        // leitura do registro
+                    } else if (linha.startsWith("1")) {
+                        final Registro registro = new Registro(linha);
+                        registros.add(registro);
+                        System.out.println(registro.getInscricaoDaEmpresa());
+                        System.out.println(new SimpleDateFormat("dd/MM/yyyy").format(registro.getVencimento().getTime()));
+                        System.out.println(registro.getFormaPagamento());
+                        System.out.println(registro.getValorTitulo());
+                        System.out.println("------------------------------------------------------");
+                    } else {
+                        final Trailer trailer = new Trailer(linha);
+                        System.out.println("CodigoRegistro :: " + trailer.getCodigoRegistro());
+                        System.out.println("Sequencial     :: " + trailer.getSequencial());
+                    }
+                }
+            }
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
     }
 }
